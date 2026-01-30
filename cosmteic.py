edc08099d1b76f269cfa5bc0beec618b238d16e2
bloramarrero <bloramarrero28@stsaviour.org>
2026-01-30 13:58:40 -0500

    lab 4 done!

--- cosmteic.py
+++ cosmteic.py
@@ -6,14 +6,16 @@ class Cosmetic:
         self.shade = shade
         self.in_stock = in_stock
 
+    # you can apply all cosmetics!
     def apply(self, location: str) -> str:
         return f'you apply {self.name} with shade {self.shade} to {location}'
     
 
 if __name__ == '__main__':
+    # this is the first instance
     lipliner = Cosmetic('Makeup Forever', '608 Wherever Walnut', True)
     print(lipliner.apply('lips'))
 
-if __name__ == '__main__':
+    # this is the second instance
     Mascara = Cosmetic ( 'Milani', 'Black', True)
-    print (Mascara.apply ('eyelashes'))
+    print (Mascara.apply('eyelashes'))
